bf03d7760e42c2965152fdac6af2516335228a6b
Demes Toss <demestoss@proton.me>
2023-10-10 07:25:46 +0200

    Nvim diagnostics

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -37,6 +37,8 @@ vim.keymap.set('n', '<C-t>', ':silent !tmux neww tt<CR>', { silent = true })
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 --[[ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
