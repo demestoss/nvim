ecf7802784b99dba2dcc3cbc964871bf1ee8cc6e
Demes Toss <demestoss@proton.me>
2023-10-18 14:05:03 +0200

    Lint fix

--- lua/demestoss/plugins/linting.lua
+++ lua/demestoss/plugins/linting.lua
@@ -5,12 +5,12 @@ return {
     local lint = require("lint")
 
     lint.linters_by_ft = {
-      javascript = { "eslint" },
-      typescript = { "eslint" },
-      javascriptreact = { "eslint" },
-      typescriptreact = { "eslint" },
-      svelte = { "eslint" },
-      vue = { "eslint" },
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      svelte = { "eslint_d" },
+      vue = { "eslint_d" },
       yaml = { "yamllint" },
     }
 
